1364c74dbfa3c46215cc9d5d15a2e65743f2d20e
Parker2107 <113683584+Parker2107@users.noreply.github.com>
2025-03-26 20:20:48 +0530

    Implemented full search with custom display

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ const API = "https://fakestoreapi.com/";
 
 document.addEventListener("DOMContentLoaded", () => {
 	loadCategories();
+	fetchProducts();
 });
+
+let productList = [];
+
+async function fetchProducts() {
+	const url = API + "products/";
+	try {
+		const response = await fetch(url);
+		productList = await response.json();
+		return productList;
+	} catch (error) {
+		console.error("Error fetching products:", error);
+		return [];
+	}
+}
 
 function loadCategories() {
 	const categories = ["electronics", "jewelery", "men's clothing", "women's clothing"];
@@ -41,7 +56,7 @@ function displayProducts(category, products) {
             <h3>${product.title}</h3>
             <div class="product-card-price">
             <p>Price => $${price}</p>
-            <button class="details-button" onclick="fetchDetails(${productId})">Add to Cart</button>
+            <button class="details-button" onclick="addToCart(${productId})">Add to Cart</button>
             </div>
         `;
 
@@ -52,7 +67,64 @@ function displayProducts(category, products) {
 	container.appendChild(categorySection);
 }
 
-function searchProducts() {
+async function searchProducts() {
 	const search = document.getElementById("searchbar").value.toLowerCase();
-	//TODO
+	const allProd = document.querySelectorAll(".category-section"); // Selecting all products
+	let matchingProds = [];
+	allProd.forEach((prod) => {
+		prod.style.display = "none";
+	});
+
+	productList.forEach((prod) => {
+		if (prod.title.toLowerCase().includes(search)) {
+			matchingProds.push(prod);
+		}
+	});
+	displaySearchResults(matchingProds);
+}
+
+function displaySearchResults(products) {
+	const container = document.getElementById("products-container");
+
+	container.innerHTML = "";
+
+	const searchResults = document.createElement("div");
+	searchResults.className = "search-results";
+
+	const searchTitle = document.createElement("h2");
+	searchTitle.textContent = "Search Results";
+
+	searchResults.appendChild(searchTitle);
+
+	const searchProdsContainer = document.createElement("div");
+	searchProdsContainer.className = "search-prod-container";
+
+	if (products.length === 0) {
+		const noResults = document.createElement("p");
+		noResults.textContent = "No products found.";
+		searchResults.appendChild(noResults);
+	} else {
+		products.forEach((search) => {
+			const searchCard = document.createElement("div");
+			searchCard.className = "search-card";
+
+			const productId = search.id;
+			var price = search.price.toFixed(2);
+			searchCard.innerHTML = `
+            <img src="${search.image}" alt="${search.title}">
+            <div class = "search-result-desc">
+			<h3>${search.title}</h3>
+			<p>${search.description}</p>
+			</div>
+            <div class="search-card-price">
+            <p>Price => $${price}</p>
+            <button class="details-button" onclick="addToCart(${productId})">Add to Cart</button>
+            </div>
+        `;
+
+			searchProdsContainer.appendChild(searchCard);
+		});
+	}
+	searchResults.appendChild(searchProdsContainer);
+	container.appendChild(searchResults);
 }
